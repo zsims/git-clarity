6a4f5627ebbf70d1cac0f311ca7964fba368b37d
satsuper <satsuper@users.noreply.github.com>
2015-10-02 17:07:05 +1000

    Add initial solution for project

---
+++ GitClarity/AppBoostrapper.cs
@@ -0,0 +1,69 @@
+﻿using Autofac;
+using Caliburn.Micro;
+using GitClarity.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Windows;
+
+namespace GitClarity
+{
+    /// <summary>
+    /// Entry point for the Caliburn.Micro framework
+    /// </summary>
+    public sealed class AppBootstrapper : BootstrapperBase
+    {
+        IContainer _container;
+
+        public AppBootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void Configure()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterType<WindowManager>()
+                   .AsImplementedInterfaces()
+                   .SingleInstance();
+
+            builder.RegisterType<EventAggregator>()
+                   .AsImplementedInterfaces()
+                   .SingleInstance();
+
+            // view models
+            builder.RegisterAssemblyTypes(Assembly.GetExecutingAssembly())
+                   .InNamespaceOf<ShellViewModel>()
+                   .AsImplementedInterfaces()
+                   .InstancePerDependency();
+
+            _container = builder.Build();
+        }
+
+        protected override object GetInstance(Type service, string key)
+        {
+            if (string.IsNullOrWhiteSpace(key))
+            {
+                return _container.Resolve(service);
+            }
+
+            return _container.ResolveNamed(key, service);
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return _container.Resolve(typeof(IEnumerable<>).MakeGenericType(service)) as IEnumerable<object>;
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            _container.InjectProperties(instance);
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<IShellViewModel>();
+        }
+    }
+}
---
+++ GitClarity/ViewModels/ShellViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Caliburn.Micro;
+using GitClarity.Properties;
+
+namespace GitClarity.ViewModels
+{
+    /// <summary>
+    /// View model for the main window
+    /// </summary>
+    interface IShellViewModel
+    {
+    }
+
+    sealed class ShellViewModel : Screen, IShellViewModel
+    {
+        public ShellViewModel()
+        {
+            DisplayName = Resources.ShellViewModel_DisplayName;
+        }
+    }
+}
